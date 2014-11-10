d3724fb57e50606d66d9c74b9fd5340d45bac882
Hernan Diaz <tron@3tech.com.ar>
2014-11-10 04:16:42 -0300

    Update membersite_config.php

--- include/membersite_config.php
+++ include/membersite_config.php
@@ -11,13 +11,12 @@ $fgmembersite->SetAdminEmail('tron@3tech.com.ar');
 
 //Provide your database login details here:
 //hostname, user name, password, database name and table name
-//note that the script will create the table (for example, fgusers in this case)
-//by itself on submitting register.php for the first time
-$fgmembersite->InitDB(/*hostname*/'localhost',
-                      /*username*/'root',
+
+$fgmembersite->InitDB(/*hostname*/'localhost', //host:port de el SQL Server
+                      /*username*/'root', // Usuario con privilegios ALL sobre BD
                       /*password*/'++++++',
-                      /*database name*/'gts',
-                      /*table name*/'User');
+                      /*database name*/'gts', // Normalmente gts
+                      /*table name*/'User'); // siempre User
 
 //For better security. Get a random string from this link: http://tinyurl.com/randstr
 // and put it here
